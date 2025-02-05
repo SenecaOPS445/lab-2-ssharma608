256f23d4c2f292f98b4f8d1d110212e8b6a4735c
Shikshya Sharma <ssharma608@myseneca.ca>
2025-02-05 18:33:37 -0500

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+#Author: Shikshya Sharma
+#Author ID: ssharma608@myseneca.ca
+
+
+#Date Created:2025/02/05
+
+
+timer = 10
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
+
